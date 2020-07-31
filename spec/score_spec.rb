dad5ecac6dcd11026d04324380a5948788420172
mimi peshy <mimipeshy@gmail.com>
2020-07-31 20:50:16 +0300

    fixed score test

--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -2,21 +2,13 @@ require_relative '../lib/score.rb'
 require_relative '../lib/player.rb'
 
 describe Score do
-    let (:score) {Score.new(player)}
-    let(:board) {[[1, 2, 3], [4, 5, 6], [7, 8, 9]]}
-    players = ['Peris', 'Alex']
-    let (:player) do 
-        player = Player.new
-        player.players.push('Peris')
-        player.players.push('Alex')
-        player 
+  let(:new_score) { Score.new(player) }
+  let(:player) {Player.new}
+  let(:board) { [[1, 2, 3], [4, 5, 6], [7, 8, 9]] }
+  
+  context 'when first created' do
+    it 'should be instance of Score' do
+      expect(new_score).to be_instance_of Score
     end
-    before { player.next_player }
-    context 'It should return the array of players' do
-        it 'Players in the game' do
-            expect(score.players).to match(players)
-        end
-
-    end
-   
-end    
+  end
+end
