d3c49f0a4155b307f8245ea51196517b46a0123b
mimi peshy <mimipeshy@gmail.com>
2020-07-31 20:01:27 +0300

    fixed switching players

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/score'
 
 describe Player do
   let(:player){Player.new}
-  let(:player_array){player.players=['Alex','Peris']}
+  let(:player_array){player.players=['Alex']}
 
     context 'this checks if subject is an instance of players' do
       it 'checks if it creates an instance of a class' do
@@ -17,11 +17,13 @@ describe Player do
       end          
     end
     context 'this returns the icons of the players' do
-      it 'returns the index of the next player in the array' do
+      it 'returns the icon of the player in the array' do
         expect(player.icon).to eq('#')
       end
+    context 'this tests the switch method'do
       it 'switches from one player to the next' do
-        expect(player.next_player).to change(player.current_player).from('Alex').to('Peris')
+        expect{player.next_player}.to change{player_array<<('Alex')}.to(player_array<<('Peris'))
       end
+    end
     end   
 end
